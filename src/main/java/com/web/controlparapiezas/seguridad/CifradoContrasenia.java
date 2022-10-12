b24aaa68ea329110336a5ec4009891ffde6787c2
Manuel Siciliano <sici1230@gmail.com>
2022-10-11 21:41:53 -0600

    Agregando las variables de entorno y los metodos para cifrar las contraseñas

--- src/main/java/com/web/controlparapiezas/seguridad/CifradoContrasenia.java
+++ src/main/java/com/web/controlparapiezas/seguridad/CifradoContrasenia.java
@@ -4,9 +4,20 @@
  */
 package com.web.controlparapiezas.seguridad;
 
-import com.web.controlparapiezas.entidades.Usuarios;
-import javax.inject.Inject;
-import javax.security.enterprise.identitystore.Pbkdf2PasswordHash;
+import com.web.controlparapiezas.VariablesDeEntorno;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.AlgorithmParameters;
+import java.security.GeneralSecurityException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
 
 /**
  *
@@ -14,13 +25,64 @@ import javax.security.enterprise.identitystore.Pbkdf2PasswordHash;
  */
 public class CifradoContrasenia {
 
-    @Inject
-    private Usuarios usuario;
+    private static CifradoContrasenia instancia;
+    
+    private VariablesDeEntorno variables = new VariablesDeEntorno();
 
-    private Pbkdf2PasswordHash passwordHash;
+    public CifradoContrasenia() {
+    }
 
-    public Usuarios build(Usuarios usuariosincifrar) {
-        usuario.setContrasenia(passwordHash.generate(usuariosincifrar.getContrasenia().toCharArray()));
-        return usuariosincifrar;
+    public String cifrar(String contrasenia) {
+        try {
+            System.out.println("contraseña " + contrasenia);
+            byte[] salt = variables.getSALT().getBytes();
+            int iterationCount = variables.getITERATION_COUNT();
+            int keyLength = variables.getKEY_LENGTH();
+            SecretKeySpec key = createSecretKey(contrasenia.toCharArray(), salt, iterationCount, keyLength);
+
+            String originalPassword = contrasenia;
+            System.out.println("Original password: " + originalPassword);
+            String encryptedPassword = encrypt(originalPassword, key);
+            System.out.println("Encrypted password: " + encryptedPassword);
+            String decryptedPassword = decrypt(encryptedPassword, key);
+            System.out.println("Decrypted password: " + decryptedPassword);
+            return encryptedPassword;
+
+        } catch (Exception e) {
+        }
+        return "";
+    }
+
+    public static SecretKeySpec createSecretKey(char[] password, byte[] salt, int iterationCount, int keyLength) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+        PBEKeySpec keySpec = new PBEKeySpec(password, salt, iterationCount, keyLength);
+        SecretKey keyTmp = keyFactory.generateSecret(keySpec);
+        return new SecretKeySpec(keyTmp.getEncoded(), "AES");
+    }
+
+    public static String encrypt(String dataToEncrypt, SecretKeySpec key) throws GeneralSecurityException, UnsupportedEncodingException {
+        Cipher pbeCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        pbeCipher.init(Cipher.ENCRYPT_MODE, key);
+        AlgorithmParameters parameters = pbeCipher.getParameters();
+        IvParameterSpec ivParameterSpec = parameters.getParameterSpec(IvParameterSpec.class);
+        byte[] cryptoText = pbeCipher.doFinal(dataToEncrypt.getBytes("UTF-8"));
+        byte[] iv = ivParameterSpec.getIV();
+        return base64Encode(iv) + ":" + base64Encode(cryptoText);
+    }
+
+    private static String base64Encode(byte[] bytes) {
+        return Base64.getEncoder().encodeToString(bytes);
+    }
+
+    public static String decrypt(String string, SecretKeySpec key) throws GeneralSecurityException, IOException {
+        String iv = string.split(":")[0];
+        String property = string.split(":")[1];
+        Cipher pbeCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        pbeCipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(base64Decode(iv)));
+        return new String(pbeCipher.doFinal(base64Decode(property)), "UTF-8");
+    }
+
+    private static byte[] base64Decode(String property) throws IOException {
+        return Base64.getDecoder().decode(property);
     }
 }
